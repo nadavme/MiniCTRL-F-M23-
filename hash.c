6add3780c0260466b9a156810027240c187966dc
Nadav Meidan <nadav.medan@mail.huji.ac.il>
2020-01-20 20:58:58 +0200

    Initial definition of linkedList and reading from files.

--- hash.c
+++ hash.c
@@ -7,7 +7,27 @@
 
 #include <stdio.h>
 
-int main() {
+#define FILE_NAME NULL
+
+typedef struct Node
+{
+    char *value;
+    struct Node *next;
+} Node;
+
+typedef struct linkedList
+{
+    Node *head;
+}linkedList;
+
+int main(int argc, char * argv[]) {
+    FILE *fp = fopen(FILE_NAME, "r");
+    if(fp == NULL)
+    {
+        fclose(fp);
+        fprintf(stderr, "𝐶𝑎𝑛𝑛𝑜𝑡 𝑜𝑝𝑒𝑛 𝑡ℎ𝑒 𝑓𝑖𝑙𝑒");
+    }
+    fclose(fp);
     printf("Hello, World!\n");
     return 0;
 }
